a763ac024918ac1cdd491f8d56dc6428724ef954
oranfish <251910646@qq.com>
2017-08-21 01:39:00 +0800

    init

--- housedata_web/src/main/java/com/oranfish/controller/HelloController.java
+++ housedata_web/src/main/java/com/oranfish/controller/HelloController.java
@@ -5,6 +5,7 @@ import com.oranfish.service.HouseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,7 +18,7 @@ public class HelloController {
     @Autowired
     private HouseService houseService;
 
-    @RequestMapping("/{name}")
+    @GetMapping("/{name}")
     public String getHello( @PathVariable("name") String name, Model model){
         String hello = helloService.getHello();
         model.addAttribute("result", hello + "，" + name);
