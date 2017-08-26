bc15c57390a855896f3406cbcc41615fbfe7b4d3
oranfish <251910646@qq.com>
2017-08-27 04:22:05 +0800

    sql

--- house.sql
+++ house.sql
@@ -1,12 +1,12 @@
 CREATE TABLE `house` (
-`id`  bigint(20) NOT NULL AUTO_INCREMENT ,
-`date`  datetime NULL ON UPDATE CURRENT_TIMESTAMP ,
-`title`  varchar(255) NULL ,
-`total_price`  decimal(20,2) NULL ,
-`unit_price`  decimal(20,2) NULL ,
-`house_detail`  varchar(255) NULL ,
-`community_detail`  varchar(255) NULL ,
-`label`  varchar(255) NULL ,
-PRIMARY KEY (`id`)
-)
-;
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `date` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `title` varchar(255) DEFAULT NULL,
+  `total_price` decimal(20,2) DEFAULT NULL,
+  `unit_price` decimal(20,2) DEFAULT NULL,
+  `house_detail` varchar(255) DEFAULT NULL,
+  `community_detail` varchar(255) DEFAULT NULL,
+  `label` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_all` (`title`,`house_detail`,`community_detail`,`label`)
+) ENGINE=InnoDB AUTO_INCREMENT=143827 DEFAULT CHARSET=utf8;
