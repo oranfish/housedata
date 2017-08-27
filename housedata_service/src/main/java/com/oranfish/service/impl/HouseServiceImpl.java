c9e222a8ce1bb14975d331c6815d6ee258a001b8
oranfish <251910646@qq.com>
2017-08-27 11:57:37 +0800

    bug

--- housedata_service/src/main/java/com/oranfish/service/impl/HouseServiceImpl.java
+++ housedata_service/src/main/java/com/oranfish/service/impl/HouseServiceImpl.java
@@ -4,6 +4,8 @@ import com.oranfish.common.DateUtils;
 import com.oranfish.dao.HouseMapper;
 import com.oranfish.entity.House;
 import com.oranfish.service.HouseService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,8 @@ public class HouseServiceImpl implements HouseService{
     @Autowired
     private HouseMapper houseMapper;
 
+    private static final Logger LOG = LoggerFactory.getLogger(HouseServiceImpl.class);
+
     public Integer getTodayCount(){
         return houseMapper.count(DateUtils.getToday(), DateUtils.getTodayAddDay(1));
     }
@@ -25,9 +29,12 @@ public class HouseServiceImpl implements HouseService{
             Iterator iterator = houseList.iterator();
             while(iterator.hasNext()){
                 House house = (House)iterator.next();
-                Integer houseCount = houseMapper.countSingleHouse(house, DateUtils.getToday(), DateUtils.getTodayAddDay(1));
-                if(houseCount != null && houseCount > 0){
-                    iterator.remove();
+                LOG.info("入库前未过滤的house：{}", house);
+                if(house != null){
+                    Integer houseCount = houseMapper.countSingleHouse(house, DateUtils.getToday(), DateUtils.getTodayAddDay(1));
+                    if(houseCount != null && houseCount > 0){
+                        iterator.remove();
+                    }
                 }
             }
             if(houseList != null && !houseList.isEmpty()){
